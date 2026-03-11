e26df6c8d606430b1640a6c3e302320d1ee267ed
Bram Bolt <bram.bolt@icloud.com>
2026-03-11 16:57:37 +0100

    add load_bn_from_base64

---
+++ src/utils/file_utils.py
@@ -0,0 +1,40 @@
+import os
+import base64
+import tempfile
+import pyagrum as gum
+
+# get loaded BN from base64
+def load_bn_from_base64(contents: str, filename: str) -> gum.BayesNet:
+    _, extension = os.path.splitext(filename)
+    extension = extension.lower().lstrip(".") # get extension
+
+    # check if extension is allowed
+    allowed_exts = gum.availableBNExts().split("|") 
+    if extension not in allowed_exts:
+        raise ValueError(
+            f"Unsupported Bayesian Network format: .{extension}. "
+            f"Supported formats: {', '.join(allowed_exts)}"
+        )
+
+    # Decode
+    try:
+        content_type, content_string = contents.split(',')
+        decoded_bytes = base64.b64decode(content_string)
+    except Exception as e:
+        raise ValueError(f"Failed to decode base64 content: {e}")
+
+    # Make a temporary file
+    fd, temp_path = tempfile.mkstemp(suffix=f".{extension}")
+
+    # load BN
+    try:
+        with os.fdopen(fd, 'wb') as tmp:
+            tmp.write(decoded_bytes)
+
+        bn = gum.loadBN(temp_path)
+        return bn
+
+    finally:
+        if os.path.exists(temp_path):
+            os.remove(temp_path)
+            
