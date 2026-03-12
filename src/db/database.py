38c5adcb39c4554fa6094bd1cfc2a1b18739d6d7
Kuit, K.M. (Kamil) <kamil.kuit@ru.nl>
2026-03-12 20:18:57 +0100

    Correct code allocation init py to database py

--- src/db/database.py
+++ src/db/database.py
@@ -1,3 +1,14 @@
+import sqlite3
+
+con = sqlite3.connect("feedback.db")
+cur = con.cursor()
+
+# CREATE TABLE
+create_table = """CREATE TABLE IF NOT EXISTS 
+feedback(method_pref, exp1_rating, exp2_rating, exp3_rating, open_comment)
+"""
+cur.execute(create_table)
+
 
 # ADD ENTRY TO FEEDBACK DATABASE SQLITE
 
