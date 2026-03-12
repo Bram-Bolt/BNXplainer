da718fb002a634c32538f2360d14fd929eef25ee
roboluc <roboluc@gmail.com>
2026-03-12 12:34:26 +0100

    ignore Ipython notebook warning

--- src/app.py
+++ src/app.py
@@ -1,15 +1,16 @@
+import warnings
+warnings.filterwarnings("ignore", module=r"pyagrum\.lib\.notebook") # Ignore Ipython warning
 from dash import Dash
 import dash_mantine_components as dmc
 from pages.home import create_layout
 
-# mannualy select rendering
+# manually select rendering
 import matplotlib
 matplotlib.use("Agg")
 
 app = Dash(__name__)
 
 app.layout = dmc.MantineProvider(create_layout())
-
 
 if __name__ == "__main__":
     app.run(debug=True)
