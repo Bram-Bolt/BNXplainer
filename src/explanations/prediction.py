1f41ffe213e4703334b8a5b141d6d84d85190cf3
OlivierBroekman <olivier.broekman@ru.nl>
2026-04-11 17:04:01 +0200

    Added documentation (in form of docstrings/comments) to voi.py, prediction.py

--- src/explanations/prediction.py
+++ src/explanations/prediction.py
@@ -6,6 +6,18 @@ def compute_prediction(
     target: str,
     evidence: dict
 ) -> dict:
+    """Computes Prediction in a Bayesian Network
+
+    Args:
+        bn (gum.BayesNet): Bayesian Network
+        target (str): Name of target variable
+        evidence (dict): Current evidence
+
+    Returns:
+        dict: returns target, states, 
+        probabilities of states, most likely state,
+        and most likely probability of that state.
+    """
     ie = gum.LazyPropagation(bn)
     ie.setEvidence(evidence)
     ie.makeInference()
@@ -14,11 +26,13 @@ def compute_prediction(
     n_states = variable.domainSize()
     states = list(variable.labels())
 
+    # Calculate posterior probability distribution
     probs = [
         ie.posterior(target)[{target: i}]
         for i in range(n_states)
     ]
 
+    # Polish posteriors
     probabilities = {
         state: round(prob, 6)
         for state, prob in zip(states, probs)
