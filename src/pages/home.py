97dc61b4801d4d8824908eaf76af107c92fc75cd
Arthur Lemke <arthur.lemke@ru.nl>
2026-04-15 16:24:45 +0200

    fix: amend home.py to use the seperated extract_bn_features function service. pytest not works across the repo from root

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,7 +1,8 @@
 import dash_mantine_components as dmc
 from dash import html, dcc, callback, Input, Output, State, no_update, clientside_callback, ctx, ALL, MATCH
 import json
-from services.inference_service import generate_inference_html, extract_bn_features
+from services.inference_service import generate_inference_html
+from services.feature_extraction import extract_bn_features
 from utils.file_utils import load_bn_from_base64
 from explanations.voi import compute_voi, voi_to_display
 from db.database import insertEntry
