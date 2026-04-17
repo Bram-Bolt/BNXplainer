9d75a729876cef153508adec523da14fc83ef88c
Bram Bolt <bram.bolt@icloud.com>
2026-04-17 12:34:16 +0200

    move upload callback

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -2,8 +2,9 @@
 from .sliders import register_slider_callbacks
 from .explanations import register_explanation_callbacks
 from .feedback import register_feedback_callbacks
-
+from .upload import register_upload_callbacks
 def register_callbacks(app):
     register_slider_callbacks(app)
     register_explanation_callbacks(app)
     register_feedback_callbacks(app)
+    register_upload_callbacks(app)
