36565767498a7ac09d09fc117590ce115dcc6612
OlivierBroekman <olivier.broekman@ru.nl>
2026-04-27 13:35:05 +0200

    Added evidence retrieval and image generation to __init__.py

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -5,6 +5,8 @@ from .feedback import register_feedback_callbacks
 from .upload import register_upload_callbacks
 from .target_node import register_target_node_callback
 from .nodeslist_rendering import register_nodeslist_rendering_callback
+from .evidence import register_evidence_callback
+from .generate_image import register_image_callback
 
 def register_callbacks(app):
     register_slider_callbacks(app)
@@ -13,3 +15,5 @@ def register_callbacks(app):
     register_upload_callbacks(app)
     register_target_node_callback(app)
     register_nodeslist_rendering_callback(app)
+    register_evidence_callback(app)
+    register_image_callback(app)
