01b73163c9d56edae04d015daa9fd3349cba4aac
OlivierBroekman <olivier.broekman@ru.nl>
2026-04-27 11:55:58 +0200

    Removed unused imports

--- src/callbacks/upload.py
+++ src/callbacks/upload.py
@@ -1,9 +1,5 @@
 # callbacks/upload_callbacks.py
-import dash_mantine_components as dmc
-
-from dash import html, dcc, callback, Input, Output, State, no_update, clientside_callback, ctx, ALL, MATCH
-from utils.file_utils import load_bn_from_base64
-from utils.inference_html import generate_inference_html
+from dash import callback, Input, Output, State, no_update, clientside_callback
 
 def register_upload_callbacks(app):
     clientside_callback(
