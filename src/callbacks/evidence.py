4e9c0cd1f70a73d4140f4056c12176f9c180f9f6
OlivierBroekman <olivier.broekman@ru.nl>
2026-05-01 19:29:06 +0200

    Reworked evidence function to accomodate new way of processing evidence

--- src/callbacks/evidence.py
+++ src/callbacks/evidence.py
@@ -1,44 +1,50 @@
 # callbacks/evidence.py
-from dash import callback, Input, Output, State, no_update, ALL
-from collections import defaultdict
+from dash import callback, Input, Output, State, ALL
 
 def register_evidence_callback(app):
     @callback(
         Output('evidence-store', 'data'),
         Input('submit-evidence', 'n_clicks'),
-        State({'type': 'binary-slider', 'node': ALL}, 'value'),
-        State({'type': 'node-slider', 'node': ALL, 'state': ALL}, 'value'),
-        State({'type': 'binary-slider', 'node': ALL}, 'id'),
-        State({'type': 'node-slider', 'node': ALL, 'state': ALL}, 'id'),
+        State({'type': 'evidence-radiogroup', 'node': ALL}, 'value'),
+        State({'type': 'evidence-radiogroup', 'node': ALL}, 'id'),
     )
-    def read_evidence(n_clicks: int, binary_values: list[float], nary_values: list[float], binary_ids: list[dict[str, str]], nary_ids: list[dict[str, str, str]]):
-        """This function reads evidence from the sliders when the evidence is submitted.
-        It then stores this evidence for later use in another callback.
+    def read_evidence(n_clicks: int, node_values: list[str | None], node_ids: list[dict[str, str]]):
+        """This function reads evidence from the radiogroup when the evidence is submitted.
+        It then stores this evidence for later use in other callbacks.
 
         Args:
             n_clicks (int): The amount of clicks of the 'submit-evidence' button this session
-            binary_values (list[float]): Values of binary sliders
-            nary_values (list[float]): Values of n-ary sliders
-            binary_ids (list[dict[str, str]]): IDs of binary sliders
-            nary_ids (list[dict[str, str, str]]): IDs of n-ary sliders
+            node_values (list[str | None]): List of observed states per node (hence None in typehint)
+            node_ids (list[dict[str, str]]): List of dictionaries of form {'type': 'evidence-radiogroup', 'node': nodename}
 
         Returns:
-            dict[str, list[float]]: Evidence, as passed through the sliders
+            dict[str, str]: Evidence, as passed through the radiogroups
         """
         evidence = {}
 
-        # Handle binary nodes
-        for val, node_id in zip(binary_values, binary_ids):
-            node = node_id.get('node')
-            evidence[node] = [round(val/100, 4), round(1-(val/100), 4)]
-        
-        # Handle nary nodes
-        nary_evidence = defaultdict(list)
-        for val, node_id in zip(nary_values, nary_ids):
-            node = node_id.get('node')
-            nary_evidence[node].append(round(val/100, 4))
+        # Construct evidence
+        for val, node_id in zip(node_values, node_ids):
+            if val == None:
+                continue
+            else:
+                node = node_id.get('node')
+                evidence[node] = val
+        print(evidence)
 
-        for node, arr in dict(nary_evidence).items():
-            evidence[node] = arr
+        # Deprecated Code DO NOT REMOVE UNTIL RELEASE
+        # This can still be used for sliders way of handling evidence
+        # Handle binary nodes
+        # for val, node_id in zip(binary_values, binary_ids):
+        #     node = node_id.get('node')
+        #     evidence[node] = [round(val/100, 4), round(1-(val/100), 4)]
+        
+        # # Handle nary nodes
+        # nary_evidence = defaultdict(list)
+        # for val, node_id in zip(nary_values, nary_ids):
+        #     node = node_id.get('node')
+        #     nary_evidence[node].append(round(val/100, 4))
+
+        # for node, arr in dict(nary_evidence).items():
+        #     evidence[node] = arr
             
         return evidence
