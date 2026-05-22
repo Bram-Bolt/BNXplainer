4c726900f709c22b7fb71e38810661015a4b4ef6
OlivierBroekman <olivier.broekman@ru.nl>
2026-05-22 08:01:34 +0200

    Fixed bug where setting evidence for a node, then setting it as target, then setting another node as target reinstates previous evidence. Additionally, evidence took priority in the model over target, this is no longer the case

--- src/callbacks/evidence.py
+++ src/callbacks/evidence.py
@@ -1,34 +1,41 @@
 # callbacks/evidence.py
-from dash import callback, Input, Output, State, ALL
+from dash import callback, Input, Output, State, ALL, ctx, no_update
 
 def register_evidence_callback(app):
     @callback(
         Output('evidence-store', 'data'),
+        Output({'type': 'evidence-radiogroup', 'node': ALL}, 'value'),
         Input('submit-evidence', 'n_clicks'),
+        Input('target-store', 'data'),
         State({'type': 'evidence-radiogroup', 'node': ALL}, 'value'),
         State({'type': 'evidence-radiogroup', 'node': ALL}, 'id'),
     )
-    def read_evidence(n_clicks: int, node_values: list[str | None], node_ids: list[dict[str, str]]):
+    def read_evidence(n_clicks: int, target: str, node_values: list[str | None], node_ids: list[dict[str, str]]):
         """This function reads evidence from the radiogroup when the evidence is submitted.
-        It then stores this evidence for later use in other callbacks.
+        It parses and then stores this evidence for later use in other callbacks.
+        Additionally, the function resets the observed state for target nodes
 
         Args:
             n_clicks (int): The amount of clicks of the 'submit-evidence' button this session
+            target (str): The current target node
             node_values (list[str | None]): List of observed states per node (hence None in typehint)
             node_ids (list[dict[str, str]]): List of dictionaries of form {'type': 'evidence-radiogroup', 'node': nodename}
 
         Returns:
             dict[str, str]: Evidence, as passed through the radiogroups
+            list[str | None]: List of observed states per node
         """
         evidence = {}
+        node_vals = []
 
         # Construct evidence
         for val, node_id in zip(node_values, node_ids):
-            if val == None:
-                continue
+            if node_id.get('node') == target or val == None:
+                node_vals.append(None)
             else:
                 node = node_id.get('node')
                 evidence[node] = val
+                node_vals.append(val)
 
         # Deprecated Code DO NOT REMOVE UNTIL RELEASE
         # This can still be used for sliders way of handling evidence
@@ -45,5 +52,4 @@ def register_evidence_callback(app):
 
         # for node, arr in dict(nary_evidence).items():
         #     evidence[node] = arr
-            
-        return evidence
+        return evidence, node_vals
